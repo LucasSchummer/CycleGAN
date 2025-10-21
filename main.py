b3664f2f7bb2c10a199465996c55b4880e5c6745
Lucas Schummer <schummerlucas22@gmail.com>
2025-10-22 01:12:40 +0200

    Implement CycleGAN

---
+++ main.py
@@ -0,0 +1,72 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import time
+from tqdm import tqdm
+from models.cyclegan import CycleGAN
+from utils.dataset import UnpairedDataset
+from utils.checkpoint_manager import CheckpointManager
+
+
+epochs = 10
+lr = 2e-4
+lbda = 10
+n_steps_log = 10
+n_epochs_log = 1
+n_epochs_checkpoint = 2
+
+run_name = "HorseZebra_1"
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+model = CycleGAN(lr, lbda, device).to(device)
+dataset = UnpairedDataset("data/horse2zebra")
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+writer = SummaryWriter(log_dir=f"runs/{run_name}")
+checkpoint_manager = CheckpointManager(f"checkpoints/{run_name}", max_checkpoints=5)
+
+step = 0
+for epoch in range(epochs):
+    start_time = time.time()
+        
+    # tqdm progress bar for batches in the epoch
+    progress_bar = tqdm(
+        enumerate(dataloader),
+        total=len(dataloader),
+        desc=f"Epoch [{epoch+1}/{epochs}]",
+        leave=False,
+        unit="batch",
+        colour="green"
+    )
+
+    for batch_idx, (batch) in progress_bar:
+
+        metrics = model.train_step(batch)
+
+        if step % n_steps_log == 0:
+            writer.add_scalar("Loss/G/Total", metrics["loss_G"], step)
+            writer.add_scalar("Loss/G/G_AB", metrics["loss_G_AB"], step)
+            writer.add_scalar("Loss/G/G_BA", metrics["loss_G_BA"], step)
+            writer.add_scalar("Loss/D/Total", metrics["loss_D"], step)
+            writer.add_scalar("Loss/D/D_A", metrics["loss_D_A"], step)
+            writer.add_scalar("Loss/D/D_B", metrics["loss_D_B"], step)
+            writer.add_scalar("Loss/Cycle", metrics["loss_cyc"], step)
+            writer.add_scalar("Discriminator_A/Real", metrics["D_A_real"], step)
+            writer.add_scalar("Discriminator_A/Fake", metrics["D_A_fake"], step)
+            writer.add_scalar("Discriminator_B/Real", metrics["D_B_real"], step)
+            writer.add_scalar("Discriminator_B/Fake", metrics["D_B_fake"], step)
+
+        step += 1
+
+    if (epoch + 1) % n_epochs_log == 0:
+        fake_A, fake_B = model.get_fake_images(batch)
+        writer.add_images("Fake/B_from_A", (fake_B + 1) / 2, epoch)
+        writer.add_images("Fake/A_from_B", (fake_A + 1) / 2, epoch)
+
+    if (epoch + 1) % n_epochs_checkpoint == 0:
+        checkpoint_manager.save(model, epoch, step)
+
+    print(f"Epoch [{epoch+1}/{epochs}] completed in {time.time() - start_time:.2f}s")
+
+# tensorboard --logdir runs/HorseZebra_1
---
+++ models/cyclegan.py
@@ -0,0 +1,121 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from models.generator import Generator
+from models.discriminator import Discriminator
+from utils.buffer import ImageBuffer
+
+
+class CycleGAN(nn.Module):
+
+    def __init__(self, lr, lbda, device):
+        super().__init__()
+        
+        self.D_A = Discriminator() # Discriminator for class A
+        self.D_B = Discriminator() # Discriminator for class B
+        self.G_AB = Generator() # Generator from class A to B
+        self.G_BA = Generator() # Generator from class B to A
+
+        self.optimizer_G = torch.optim.Adam(list(self.G_AB.parameters()) + list(self.G_BA.parameters()), lr = lr)
+        self.optimizer_D = torch.optim.Adam(list(self.D_A.parameters()) + list(self.D_B.parameters()), lr = lr)
+
+        self.buffer_A = ImageBuffer(50, device)
+        self.buffer_B = ImageBuffer(50, device)
+
+        self.lbda = lbda
+        self.global_step = 0
+        self.epoch = 0
+        self.device = device
+
+    def train_step(self, batch):
+
+        real_A = batch["A"].to(self.device)
+        real_B = batch["B"].to(self.device)
+
+        fake_A = self.G_BA(real_B)
+        fake_B = self.G_AB(real_A)
+
+        cyc_A = self.G_BA(fake_B)
+        cyc_B = self.G_AB(fake_A)
+
+        # Train Generators
+
+        self.set_requires_grad([self.D_A, self.D_B], False) # No gradient for discriminators while optimizing generators
+
+        pred_fake_A = self.D_A(fake_A)
+        pred_fake_B = self.D_B(fake_B)
+
+        loss_G_AB = F.mse_loss(pred_fake_B, torch.ones_like(pred_fake_B))
+        loss_G_BA = F.mse_loss(pred_fake_A, torch.ones_like(pred_fake_A))
+        loss_cyc = F.l1_loss(real_A, cyc_A) + F.l1_loss(real_B, cyc_B)
+
+        loss_G = loss_G_AB + loss_G_BA + self.lbda * loss_cyc
+
+        self.optimizer_G.zero_grad()
+        loss_G.backward()
+        self.optimizer_G.step()
+
+        # Train Discriminators
+
+        self.set_requires_grad([self.D_A, self.D_B], True)
+
+        # Insert half of the images to the buffers and get a mixed batch (half new, half old)
+        mixed_fake_A = self.buffer_A.insert(fake_A)
+        mixed_fake_B = self.buffer_B.insert(fake_B)
+
+        pred_real_A = self.D_A(real_A)
+        pred_real_B = self.D_B(real_B)
+
+        loss_D_A = F.mse_loss(pred_real_A, torch.ones_like(pred_real_A)) + F.mse_loss(self.D_A(mixed_fake_A.detach()), torch.zeros_like(pred_real_A))
+        loss_D_B = F.mse_loss(pred_real_B, torch.ones_like(pred_real_B)) + F.mse_loss(self.D_B(mixed_fake_B.detach()), torch.zeros_like(pred_real_B))
+
+        loss_D = .5 * (loss_D_A + loss_D_B)
+
+        self.optimizer_D.zero_grad()
+        loss_D.backward()
+        self.optimizer_D.step()
+
+        return {
+            "loss_G": loss_G.item(),
+            "loss_D": loss_D.item(),
+            "loss_G_AB": loss_G_AB.item(),
+            "loss_G_BA": loss_G_BA.item(),
+            "loss_D_A": loss_D_A.item(),
+            "loss_D_B": loss_D_B.item(),
+            "loss_cyc": loss_cyc.item(),
+            "D_A_real": pred_real_A.mean().item(),
+            "D_A_fake": pred_fake_A.mean().item(),
+            "D_B_real": pred_real_B.mean().item(),
+            "D_B_fake": pred_fake_B.mean().item()
+        }
+    
+    def set_requires_grad(self, nets, requires_grad):
+
+        for net in nets:
+            for param in net.parameters():
+                param.requires_grad = requires_grad
+    
+    def get_fake_images(self, batch):
+
+        with torch.no_grad():
+            real_A = batch["A"]
+            real_B = batch["B"]
+
+            fake_B = self.G_AB(real_A)
+            fake_A = self.G_BA(real_B)
+
+            return fake_A, fake_B
+        
+    def get_state(self):
+        return {
+            "G_AB": self.G_AB.state_dict(),
+            "G_BA": self.G_BA.state_dict(),
+            "D_A": self.D_A.state_dict(),
+            "D_B": self.D_B.state_dict(),
+            "optimizer_G": self.optimizer_G.state_dict(),
+            "optimizer_D": self.optimizer_D.state_dict(),
+            "buffer_A": self.buffer_A.buffer,
+            "buffer_B": self.buffer_B.buffer,
+            "step": self.global_step,
+            "epoch": self.epoch
+        }
